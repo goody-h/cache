84ce330fb35054f980c5c61b21dc049a26d4e751
Goodhope Ordu <goodhopeordu@yahoo.com>
2022-05-29 22:46:14 +0100

    implement stack

--- main.py
+++ main.py
@@ -1,38 +1,91 @@
 
-
 class LRUCache (object):
     
     def __init__(self) -> None:
-        self.cache_size = 5
+        self.cache_size = 3
         self.items = {}
-        self.head = None
-        self.tail = None
-        pass
+        self.first = None
+        self.last = None
 
     def get(self, key):
-        pass
+        item = self.items.get(key)
+        if item != None:
+            previous = item['previous']
+            next = item['next']
+            if previous != None:
+                self.items[previous]['next'] = next
+            if next != None:
+                self.items[next]['previous'] = previous
+
+            self.items[key]['previous'] = None
+            if self.first != key:
+                self.items[key]['next'] = self.first
+                self.items[self.first]['previous'] = key
+            self.first = key
+            if previous != None and self.items[previous]['next'] == None:
+                self.last = previous
+        return item
+
 
     def put(self, key, value):
-        if self.items.get(key) != None:
-            head = self.items.get(key)['head']
-            tail = self.items.get(key)['tail']
-
-            if head != None:
-                head['tail'] = tail
-            if tail != None:
-                tail['head'] = head
+        item = self.items.get(key)
+        previous = None
+        if  item != None:
+            previous = item['previous']
+            next = item['next']
+            if previous != None:
+                self.items[previous]['next'] = next
+            if next != None:
+                self.items[next]['previous'] = previous
         elif len(self.items) == self.cache_size:
-            
-            pass
+            previous = self.items[self.last]['previous']
+            self.items[previous]['next'] = None
+            del self.items[self.last]
 
-        self.items[key] = {'value': value, 'head': None, 'tail': self.head}
-        if self.head != None:
-            self.head['head'] = key
+        self.items[key] = {'value': value, 'previous': None, 'next': self.first}
+        if self.first != None:
+            self.items[self.first]['previous'] = key
 
-        self.head = key
+        self.first = key
+        if self.last == None:
+            self.last = key
 
-        if self.tail == None:
-            self.tail = key
-            
-        pass
+        if previous != None and self.items[previous]['next'] == None:
+            self.last = previous
+
+    def prt(self):
+        if self.first != None:
+            stack = [self.first]
+            next = self.items[self.first]['next']
+            while next != None:
+                stack.append(next)
+                next = self.items[next]['next']
+            print(stack)
+        print(self.items)
+
+lru = LRUCache()
+
+lru.get('a') # None
+
+lru.put('a', 1)
+lru.put('b', 2)
+lru.put('c', 3)
+
+lru.prt() # c, b, a
+
+lru.put('a', 3)
+
+lru.prt() # a, c, b
+
+lru.get('x')
+
+lru.prt() # a, c, b
+
+lru.get('b')
+
+lru.prt() # b, a, c
+
+lru.put('d', 4)
+
+lru.prt() # d, b, a
 
